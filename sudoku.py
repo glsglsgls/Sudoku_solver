c2c2771d1da8718c796a6f7af12d196a33781ee7
Sergei Glazkov <s.glazkov93@gmail.com>
2021-04-27 18:12:10 +0300

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,9 +1,12 @@
 import numpy as np
 import random
+import tkinter
+from window import getboard
+
 class sudoku:
 
     def __init__(self):
-        self.board = np.array([[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]])
+        self.board = np.array([[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]])
         self.startList = [1,2,3,4,5,6,7,8,9]
 
     def readboard(self, board_input):
@@ -21,15 +24,15 @@ class sudoku:
                         while count < 10:
                             if possible_list.__contains__(list[-1]):
                                 self.board[x, y] = list.pop()
-                                print("new row")
-                                print(self.board)
+                                #print("new row")
+                                #print(self.board)
                                 break
                             else:
-                                print("new row")
-                                print(self.board)
+                                #print("new row")
+                                #print(self.board)
                                 self.movelist(list)
                             count += 1
-        print(self.board)
+        getboard(self.board)
     
     def movelist(self, list):
         list.append(list.pop(list.index(list[0])))
@@ -67,10 +70,6 @@ class sudoku:
     
 
         
-        
-
-
-
         
 
 
